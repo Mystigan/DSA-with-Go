5858fda29d7d2a5f46436640f02a3cf87c0b370d
Jobin John <jobin1094@gmail.com>
2020-10-04 22:07:53 +0530

    Added ability to select sorting order and unit test to support it.
    Signed-off-by: Jobin John <jobin1094@gmail.com>

--- Algorithms/sorts/merge/mergeSort.go
+++ Algorithms/sorts/merge/mergeSort.go
@@ -1,9 +1,19 @@
 package merge
 
-func Sort(input []int) []int {
+import (
+	"errors"
+	"strings"
+)
+
+// Sort takes a slice of integers, a sorting order and returns a slice sorted using the merge sort algorithm or an error if the sorting order is invalid.
+// order can have values: \"asc\" or \"desc\" (irrespective of case).
+func Sort(input []int, order string) ([]int, error) {
+	if strings.ToLower(order) != "asc" && strings.ToLower(order) != "desc" {
+		return nil, errors.New("invalid sorting order")
+	}
 	n := len(input)
 	if n < 2 {
-		return input
+		return input, nil
 	}
 	left := []int{}
 	for i := 0; i < n/2; i++ {
@@ -13,22 +23,35 @@ func Sort(input []int) []int {
 	for i := n / 2; i < n; i++ {
 		right = append(right, input[i])
 	}
-	Sort(left)
-	Sort(right)
-	return merge(left, right, input)
+	Sort(left, order)
+	Sort(right, order)
+	return merge(left, right, input, order), nil
 }
 
-func merge(left, right, input []int) []int {
+func merge(left, right, input []int, order string) []int {
 	i, j, k, nL, nR := 0, 0, 0, len(left), len(right)
-	for i < nL && j < nR {
-		if left[i] < right[j] {
-			input[k] = left[i]
-			i++
-		} else {
-			input[k] = right[j]
-			j++
+	if strings.ToLower(order) == "asc" {
+		for i < nL && j < nR {
+			if left[i] < right[j] {
+				input[k] = left[i]
+				i++
+			} else {
+				input[k] = right[j]
+				j++
+			}
+			k++
 		}
-		k++
+	} else {
+		for i < nL && j < nR {
+			if left[i] > right[j] {
+				input[k] = left[i]
+				i++
+			} else {
+				input[k] = right[j]
+				j++
+			}
+			k++
+		}
 	}
 	for i < nL {
 		input[k] = left[i]
--- Algorithms/sorts/merge/mergeSort_test.go
+++ Algorithms/sorts/merge/mergeSort_test.go
@@ -1,21 +1,32 @@
 package merge
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestSort(t *testing.T) {
-	t.Run("test one", func(t *testing.T) {
+	t.Run("test asc", func(t *testing.T) {
 		want := []int{1, 2, 3, 4, 5}
-		got := Sort([]int{4, 2, 3, 5, 1})
+		got, _ := Sort([]int{4, 2, 3, 5, 1}, "asc")
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got: %v, want: %v", got, want)
 		}
 	})
-	t.Run("test two", func(t *testing.T) {
-		want := []int{1, 2, 3, 4, 5}
-		got := Sort([]int{5, 4, 3, 2, 1})
+	t.Run("test desc", func(t *testing.T) {
+		want := []int{5, 4, 3, 2, 1}
+		got, _ := Sort([]int{1, 2, 4, 3, 5}, "desc")
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got: %v, want: %v", got, want)
+		}
+	})
+	t.Run("test invalid order", func(t *testing.T) {
+		want := []int(nil)
+		got, err := Sort([]int{1, 2, 4, 3, 5}, "descending")
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+		}
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got: %v, want: %v", got, want)
 		}
