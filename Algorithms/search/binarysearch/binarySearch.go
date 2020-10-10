2a09678d1c00bd9c70e78f81bd12c6048d2bb452
Jobin John <jobin1094@gmail.com>
2020-10-11 00:07:49 +0530

    Added iterative implementation of binary search algorithm.
    Signed-off-by: Jobin John <jobin1094@gmail.com>

--- Algorithms/search/binarysearch/binarySearch.go
+++ Algorithms/search/binarysearch/binarySearch.go
@@ -3,7 +3,8 @@ package binarysearch
 // BinarySearch takes in a slice of integers and a target integer. Searches for the target using binary search algorithm.
 // If target is found, returns its index. Otherwise returns -1.
 func BinarySearch(nums []int, target int) int {
-	return recursiveSearch(nums, 0, len(nums)-1, target)
+	// return recursiveSearch(nums, 0, len(nums)-1, target)
+	return iterativeSearch(nums, target)
 }
 
 func recursiveSearch(nums []int, left, right, target int) int {
@@ -16,6 +17,22 @@ func recursiveSearch(nums []int, left, right, target int) int {
 			return recursiveSearch(nums, left, middle-1, target)
 		}
 		return recursiveSearch(nums, middle+1, right, target)
+	}
+	return -1
+}
+
+func iterativeSearch(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for right >= left {
+		middle := left + (right-left)/2
+		if target == nums[middle] {
+			return middle
+		}
+		if target < nums[middle] {
+			right = middle - 1
+			continue
+		}
+		left = middle + 1
 	}
 	return -1
 }
