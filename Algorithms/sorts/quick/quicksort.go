3dda46a48322950924c444c4a5ba522064913360
Nikhil Reddy Gudi <nikhilreddydev@gmail.com>
2022-11-25 18:33:43 +0530

    pick random pivot value to partition the slice

--- Algorithms/sorts/quick/quicksort.go
+++ Algorithms/sorts/quick/quicksort.go
@@ -1,6 +1,15 @@
 package quick
 
-// Sort takesin a slice of integers, its start and end indices and returns a slice sorted using the quick sort algorithm.
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Sort takes in a slice of integers, its start and end indices and returns a slice sorted using the quick sort algorithm.
 func Sort(input []int, start, end int) []int {
 	if start < end {
 		pivotIndex := partition(input, start, end)
@@ -12,13 +21,20 @@ func Sort(input []int, start, end int) []int {
 
 func partition(input []int, start, end int) int {
 	partitionIndex := start
-	randomPivotIndex := end
+
+	// get a random index in the range [start, end]
+	randomPivotIndex := start + rand.Intn(end - start + 1)
+
+	// swap it with end value
+	input[randomPivotIndex], input[end] = input[end], input[randomPivotIndex]
+
+	// accumulate lesser values at front
 	for i := start; i < end; i++ {
-		if input[i] <= input[randomPivotIndex] {
+		if input[i] <= input[end] {
 			input[partitionIndex], input[i] = input[i], input[partitionIndex]
 			partitionIndex++
 		}
 	}
-	input[partitionIndex], input[randomPivotIndex] = input[randomPivotIndex], input[partitionIndex]
+	input[partitionIndex], input[end] = input[end], input[partitionIndex]
 	return partitionIndex
 }
